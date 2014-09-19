7fa79441366f0e373a2719b45ee3f69506f7c901
Verg <osyaroslav@gmail.com>
2014-09-19 18:32:47 +0300

    hg

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -71,6 +71,8 @@ class MessagesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
       params.require(:message).permit(:body)
+      puts current_user.id
+      puts @topic.id
       params.merge({message: {user_id: current_user.id, topic_id: @topic.id}})
       puts params
       params
